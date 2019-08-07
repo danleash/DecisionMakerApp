f09470c8d48770c84f51a3303522d4ffb0a6cb78
Daniel Leash <dan.leash0596@gmail.com>
2019-08-07 19:47:53 -0400

    Initial commit. I created a method with my algorithm to allow the user to input how many choices they have then input for each choice. Then using the Random() method, I chose randomly the index of one of the choice inputed into an array and displayed it to the screen.

---
+++ DecisionMakerApp/DecisionMakerApp/ChoiceInput.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecisionMakerApp
+{
+    class ChoiceInput
+    {
+        public void Choices()
+        {
+            Console.WriteLine("Enter a number of choices you want me to pick from:");
+
+            int x = Convert.ToInt32(Console.ReadLine());
+            string[] choices = new string[x];
+            for (int i = 0; i < choices.Length; i++)
+            {
+                Console.WriteLine($"Enter choice {i + 1}:");
+                choices[i] = Console.ReadLine();
+            }
+            RandomDecision(choices);
+        }
+        public void RandomDecision(string[] choices)
+        {
+            Random rand = new Random();
+            int index = rand.Next(choices.Length);
+            Console.WriteLine($"Your randomly chosen decision is: {choices[index]}");
+            Console.ReadLine();
+        }
+    }
+}
+
